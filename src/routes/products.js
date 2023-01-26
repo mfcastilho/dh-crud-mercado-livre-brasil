fbb64a54c600c16fe05cf19101bf5ff7886dcc4a
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2023-01-25 23:12:12 -0300

    Update .gitignore

---
+++ src/routes/products.js
@@ -0,0 +1,28 @@
+// ************ Require's ************
+const express = require('express');
+const router = express.Router();
+
+// ************ Controller Require ************
+const productsController = require('../controllers/productsController');
+
+/*** GET ALL PRODUCTS ***/ 
+router.get('/products', productsController.index); 
+
+/*** CREATE ONE PRODUCT ***/ 
+router.get('/products/create', productsController.create); 
+router.post('/products/create', productsController.store); 
+
+
+/*** GET ONE PRODUCT ***/ 
+router.get('/products/:id', productsController.detail); 
+
+/*** EDIT ONE PRODUCT ***/ 
+router.get('/products/edit/:id', productsController.edit); 
+router.put('/products/edit/:id', productsController.update); 
+
+
+/*** DELETE ONE PRODUCT***/ 
+router.delete('/products/:id', productsController.destroy); 
+
+
+module.exports = router;
